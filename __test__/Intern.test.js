2850e77525b5fe828d32d08e893c021edcf778da
Shagens <Hagensshik@gmail.com>
2021-12-08 18:08:04 -0500

    updates to tests and html

--- __test__/Intern.test.js
+++ __test__/Intern.test.js
@@ -1,7 +1,7 @@
 const Intern = require('../lib/Intern.js');
 
 test('creates Intern object', () => {
-    const intern = new Intern('kayla', 3, 'kayla@email.com', 'uncc');
+    const intern = new Intern('kayla', '3', 'kayla@email.com', 'uncc', 'role');
   
     expect(intern.name).toEqual(expect.any(String));
     expect(intern.id).toEqual(expect.any(Number));
@@ -11,30 +11,30 @@ test('creates Intern object', () => {
   
   test('name', () => {
     const name = 'kayla';
-    const intern = new Intern(name, 3, 'kayla@email.com', 'uncc');
+    const intern = new Intern(name, '3', 'kayla@email.com', 'uncc', 'role');
     expect(intern.name).toEqual(name);
   });
   
   test('id', () => {
     const id = '3';
-    const intern = new Intern('kayla', id, 'kayla@email.com', 'uncc');
+    const intern = new Intern('kayla', id, 'kayla@email.com', 'uncc', 'role');
     expect(intern.id).toEqual(id);
   });
   
   test('email', () => {
     const email = 'kayla@email.com';
-    const intern = new Intern('kayla', 3, email, 'uncc');
+    const intern = new Intern('kayla', '3', email, 'uncc', 'role');
     expect(intern.getEmail()).toEqual(email);
   });
 
   test('school', () => {
     const school = 'uncc';
-    const intern = new Intern('kayla', 3, 'kayla@email.com', school);
+    const intern = new Intern('kayla', '3', 'kayla@email.com', school, 'role');
     expect(intern.getSchool()).toEqual(school);
   });
   
   test('role', () => {
     const role = 'Intern';
-    const intern = new Intern('kayla', 3, 'kayla@email.com', 'uncc');
+    const intern = new Intern('kayla', '3', 'kayla@email.com', 'uncc', role);
     expect(intern.getRole()).toEqual(role);
   });
